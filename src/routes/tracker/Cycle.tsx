490245be6d35bc52692828deb2fa0269c2eb39ff
0xLil14n <0xLil14n@gmail.com>
2022-12-29 18:09:08 -0500

    modifying the shape of the data

--- src/routes/tracker/Cycle.tsx
+++ src/routes/tracker/Cycle.tsx
@@ -1,50 +1,183 @@
 import {
   component$,
+  Signal,
+  useClientEffect$,
   useSignal,
   useStore,
   useStylesScoped$,
 } from '@builder.io/qwik';
 import styles from './tracker.css?inline';
 
-interface CycleProps {
-  cycleLength: number;
-  periodLength: number;
+interface Cycle {
+  date: string;
+  symptoms: boolean;
+  isPeriod: boolean;
+  isPeriodPredicted: boolean;
+  isOvulationPredicted: boolean;
 }
+export const onScroll = (
+  r: Signal<Element | undefined>,
+  activeDate: string
+) => {
+  const rect = r.value?.getBoundingClientRect();
 
-export default component$(({ cycleLength, periodLength }: CycleProps) => {
+  const mid = (rect?.left ?? 0) + (rect?.width ?? 0) / 2;
+  [...(r.value?.children ?? [])].forEach((el: Element) => {
+    const left = el.getBoundingClientRect().left ?? 0;
+    if (Math.abs(left - mid) < 32) {
+      activeDate = el.id;
+    }
+  });
+  return activeDate;
+};
+
+export const cycle: Cycle[] = [
+  {
+    date: 'December 21, 2022',
+    symptoms: true,
+    isPeriod: true,
+    isPeriodPredicted: true,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 22, 2022',
+    symptoms: true,
+    isPeriod: true,
+    isPeriodPredicted: true,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 23, 2022',
+    symptoms: true,
+    isPeriod: true,
+    isPeriodPredicted: true,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 24, 2022',
+    symptoms: true,
+    isPeriod: true,
+    isPeriodPredicted: true,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 25, 2022',
+    symptoms: false,
+    isPeriod: true,
+    isPeriodPredicted: true,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 26, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 27, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 28, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 29, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 30, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'December 31, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'January 1, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'January 2, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'January 3, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+  {
+    date: 'January 4, 2022',
+    symptoms: false,
+    isPeriod: false,
+    isPeriodPredicted: false,
+    isOvulationPredicted: false,
+  },
+];
+export default component$(() => {
   useStylesScoped$(styles);
+  const today = new Date();
   const ref = useSignal<Element>();
-  const store = useStore({ activeIndex: 6 });
+  const store = useStore({
+    activeIndex: 6,
+    activeDate: `${today.toLocaleDateString('default', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    })}`,
+  });
+
+  useClientEffect$(({ track }) => {
+    const r = track(() => ref);
+    const activeDate = track(() => store.activeDate);
+    store.activeDate = onScroll(r, activeDate);
+  });
 
   return (
     <>
-      cycle day {store.activeIndex}
+      {store.activeDate}
       <div
         ref={ref}
         class="cycle"
         onScroll$={() => {
-          const rect = ref.value?.getBoundingClientRect();
-          const mid = (rect?.left ?? 0) + (rect?.width ?? 0) / 2;
-          [...(ref.value?.children ?? [])].forEach((el: Element, i) => {
-            const left = el.getBoundingClientRect().left ?? 0;
-
-            if (Math.abs(left - mid) < 32) {
-              store.activeIndex = i;
-            }
-          });
+          console.log('store.activeDate:', store.activeDate);
+          store.activeDate = onScroll(ref, store.activeDate);
         }}
       >
-        <h3>cycle day {store.activeIndex}</h3>
-        {[...new Array(cycleLength).keys()].map((p, i) => (
+        {cycle.map((p, i) => (
           <div
-            class={`day  ${i < periodLength ? 'period' : ''} ${
-              i === store.activeIndex ? 'today' : ''
+            id={p.date}
+            class={`day  ${p.isPeriod ? 'period' : ''} ${
+              p.date === store.activeDate ? 'today' : ''
             }`}
           >
             {i}
           </div>
         ))}
-        {/* </div> */}
       </div>
     </>
   );
