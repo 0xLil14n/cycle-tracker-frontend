03729736f0d645307bda69e61a99e369ef26ec03
0xLil14n <0xLil14n@gmail.com>
2022-12-28 19:30:19 -0500

    adding date

--- src/routes/tracker/Cycle.tsx
+++ src/routes/tracker/Cycle.tsx
@@ -17,30 +17,35 @@ export default component$(({ cycleLength, periodLength }: CycleProps) => {
   const store = useStore({ activeIndex: 6 });
 
   return (
-    <div
-      ref={ref}
-      class="cycle"
-      onScroll$={() => {
-        const rect = ref.value?.getBoundingClientRect();
-        const mid = (rect?.left ?? 0) + (rect?.width ?? 0) / 2;
-        [...(ref.value?.children ?? [])].forEach((el: Element, i) => {
-          const left = el.getBoundingClientRect().left ?? 0;
+    <>
+      cycle day {store.activeIndex}
+      <div
+        ref={ref}
+        class="cycle"
+        onScroll$={() => {
+          const rect = ref.value?.getBoundingClientRect();
+          const mid = (rect?.left ?? 0) + (rect?.width ?? 0) / 2;
+          [...(ref.value?.children ?? [])].forEach((el: Element, i) => {
+            const left = el.getBoundingClientRect().left ?? 0;
 
-          if (Math.abs(left - mid) < 32) {
-            store.activeIndex = i;
-          }
-        });
-      }}
-    >
-      {[...new Array(cycleLength).keys()].map((p, i) => (
-        <div
-          class={`day  ${i < periodLength ? 'period' : ''} ${
-            i === store.activeIndex ? 'today' : ''
-          }`}
-        >
-          {i}
-        </div>
-      ))}
-    </div>
+            if (Math.abs(left - mid) < 32) {
+              store.activeIndex = i;
+            }
+          });
+        }}
+      >
+        <h3>cycle day {store.activeIndex}</h3>
+        {[...new Array(cycleLength).keys()].map((p, i) => (
+          <div
+            class={`day  ${i < periodLength ? 'period' : ''} ${
+              i === store.activeIndex ? 'today' : ''
+            }`}
+          >
+            {i}
+          </div>
+        ))}
+        {/* </div> */}
+      </div>
+    </>
   );
 });
