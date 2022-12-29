2a948537e4bec66a9cfff4c26f292f66be4bb4a5
0xLil14n <0xLil14n@gmail.com>
2022-12-28 19:22:51 -0500

    adding animation part 1

--- src/routes/tracker/Cycle.tsx
+++ src/routes/tracker/Cycle.tsx
@@ -1,25 +1,46 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import {
+  component$,
+  useSignal,
+  useStore,
+  useStylesScoped$,
+} from '@builder.io/qwik';
 import styles from './tracker.css?inline';
 
 interface CycleProps {
   cycleLength: number;
   periodLength: number;
-  today?: number;
 }
 
-export default component$(
-  ({ cycleLength, periodLength, today }: CycleProps) => {
-    useStylesScoped$(styles);
-    return (
-      <div class="cycle">
-        {[...new Array(cycleLength).keys()].map((p, i) => (
-          <div
-            class={`day  ${i < periodLength ? 'period' : ''} ${
-              today === i ? 'today' : ''
-            }`}
-          />
-        ))}
-      </div>
-    );
-  }
-);
+export default component$(({ cycleLength, periodLength }: CycleProps) => {
+  useStylesScoped$(styles);
+  const ref = useSignal<Element>();
+  const store = useStore({ activeIndex: 6 });
+
+  return (
+    <div
+      ref={ref}
+      class="cycle"
+      onScroll$={() => {
+        const rect = ref.value?.getBoundingClientRect();
+        const mid = (rect?.left ?? 0) + (rect?.width ?? 0) / 2;
+        [...(ref.value?.children ?? [])].forEach((el: Element, i) => {
+          const left = el.getBoundingClientRect().left ?? 0;
+
+          if (Math.abs(left - mid) < 32) {
+            store.activeIndex = i;
+          }
+        });
+      }}
+    >
+      {[...new Array(cycleLength).keys()].map((p, i) => (
+        <div
+          class={`day  ${i < periodLength ? 'period' : ''} ${
+            i === store.activeIndex ? 'today' : ''
+          }`}
+        >
+          {i}
+        </div>
+      ))}
+    </div>
+  );
+});
